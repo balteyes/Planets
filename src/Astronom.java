b04aee820a5eab3c0ffe79f8a1f2a03ee88465f0
balteyes <julian.ivanova@gmail.com>
2024-09-25 16:58:35 +0300

    добавлен Астроном

--- src/Astronom.java
+++ src/Astronom.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class Astronom implements Comparator<Planet> {
     @Override
     public int compare(Planet p1, Planet p2) {
-        return Double.compare(p1.weight, p2.weight);
+        return Double.compare(p1.getWeight(), p2.getWeight());
     }
 }
